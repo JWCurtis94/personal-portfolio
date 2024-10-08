0d477858f56d78e136f18f3c41f443d9c5f30ce7
James Curtis <jameswcurtis94@outlook.com>
2024-10-08 21:45:02 +0000

    Updated Skills

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ if(isset($_POST['submit'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Complete Responsive Personal Portfolio Website Design</title>
+   <title>Personal Portfolio</title>
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
